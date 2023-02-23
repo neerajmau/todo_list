dc0b69fd68e39b250511af035e5af8b7bba1ceb8
neeraj <neeraj.maurya@innovationm.com>
2023-02-23 17:36:04 +0530

    improve UI

--- src/component/todoList/TodoList.js
+++ src/component/todoList/TodoList.js
@@ -50,33 +50,59 @@ const TodoList = () => {
         </div>
 
         <div className={style.list}>
-          <div className={style.list_header}>
-            <div>Title</div>
-            <div>Completion Status</div>
-          </div>
+          {list?.length > 0 && (
+            <div className={style.list_header}>
+              <div>Title</div>
+              <div>Completion Status</div>
+            </div>
+          )}
           <div className={style.list_cantainer}>
             {list?.map((e, index) => {
               return (
                 <div className={style.list_data} key={index}>
                   {e?.titleTogle ? (
-                    <span
-                      className={style.title}
-                      onDoubleClick={() => doubleHandle(index)}
-                    >
-                      {e.title}
-                    </span>
+                    <>
+                      <div
+                        className={style.title}
+                        onDoubleClick={() => doubleHandle(index)}
+                      >
+                        {e.title}
+                      </div>
+                      <input
+                        type="checkbox"
+                        id={`status${index}`}
+                        className={style.vh}
+                        checked={e.compalition}
+                        onChange={() => {
+                          let data = list;
+                          data[index].compalition = !data[index].compalition;
+                          saveLocalStorage(data);
+                          setList([...data]);
+                        }}
+                      />
+                      <label
+                        htmlFor={`status${index}`}
+                        style={{
+                          color: "black",
+                          marginRight: "1rem",
+                        }}
+                        className={style.label}
+                      >
+                        {e.compalition ? "Completed" : "Incomplete"}
+                      </label>
+                    </>
                   ) : (
                     <div className={style.edit_title}>
                       <input
                         type="text"
                         placeholder="Edit Title"
                         value={e.title}
-                        onBlur={() => {
-                          let data = list;
-                          data[index].titleTogle = !data[index].titleTogle;
-                          saveLocalStorage(data);
-                          setList([...data]);
-                        }}
+                        // onBlur={() => {
+                        //   let data = list;
+                        //   data[index].titleTogle = !data[index].titleTogle;
+                        //   saveLocalStorage(data);
+                        //   setList([...data]);
+                        // }}
                         onChange={(e) => {
                           let data = list;
                           data[index].title = e.target.value;
@@ -84,31 +110,19 @@ const TodoList = () => {
                           setList([...data]);
                         }}
                       />
+                      <Button
+                        type="add"
+                        onClick={() => {
+                          let data = list;
+                          data[index].titleTogle = !data[index].titleTogle;
+                          saveLocalStorage(data);
+                          setList([...data]);
+                        }}
+                      >
+                        Save
+                      </Button>
                     </div>
                   )}
-
-                  <input
-                    type="checkbox"
-                    id={`status${index}`}
-                    className={style.vh}
-                    checked={e.compalition}
-                    onChange={() => {
-                      let data = list;
-                      data[index].compalition = !data[index].compalition;
-                      saveLocalStorage(data);
-                      setList([...data]);
-                    }}
-                  />
-                  <label
-                    htmlFor={`status${index}`}
-                    style={{
-                      color: "black",
-                      marginRight: "1rem",
-                    }}
-                    className={style.label}
-                  >
-                    {e.compalition ? "Completed" : "Incomplete"}
-                  </label>
                 </div>
               );
             })}
